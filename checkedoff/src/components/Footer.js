b4ccfe77733d93322d02af521ac14a2d709a1174
Cody Hinz <codyhinz@gmail.com>
2024-10-23 01:34:39 -0400

    Small Style change for footer.

--- checkedoff/src/components/Footer.js
+++ checkedoff/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => (
   <footer className="footer">
     <div className="footer-content">
       <Link to="/login" className="footer-link">Login</Link>
-      {' / '}
+      <span className="footer-separator">/</span>
       <Link to="/signup" className="footer-link">Signup</Link>
     </div>
   </footer>
